11e27791702a6994345d8f3b846add7e13dd0dab
gabbail <gabrielbaillargeonsd@gmail.com>
2024-11-09 14:54:51 -0500

    Added a Knn classifier

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import load_dataset
+from KNNClassifier import KNNClassifier
 
 train, train_labels, test, test_labels = load_dataset.load_iris_dataset(80)
+
+knn = KNNClassifier(k=3)
+knn.fit(train, train_labels)
+
+predictions = [knn.predict(point) for point in test]
+
+print(predictions)
